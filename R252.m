eec103f9c7d23cd612c103c2be0616e039110d71
No-raa <126116833+No-raa@users.noreply.github.com>
2023-10-11 20:40:41 +1100

    Robot summon

---
+++ R252.m
@@ -0,0 +1,11 @@
+%% R252 (robot) summoning
+
+
+function obj = R252(inputArg1,inputArg2)
+obj.Property1 = inputArg1 + inputArg2;
+end
+
+function outputArg = method1(obj,inputArg)
+outputArg = obj.Property1 + inputArg;
+end 
+
