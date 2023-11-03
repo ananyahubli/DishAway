b4926cee00592bd74d14d227af828f56de703a7d
nora <blue2318514@gmail.com>
2023-11-03 15:50:05 +1100

    Safe environment

--- Environment.m
+++ Environment.m
@@ -1,64 +1,27 @@
 %% Environment
-
 classdef Environment < handle
-    
     properties
     end
-
     methods
- 
         function self = Environment()
             disp('Setting up environment');
-
             kV = [0, 0, 0];
-
             surf([-6 + kV(1),-6 + kV(1);6 + kV(1), 6 + kV(1)] ...
                 ,[-2 + kV(2),6 + kV(2);-2 + kV(2), 6 + kV(2)] ...
                 ,[0.01 + kV(3), 0.01 + kV(3); 0.01 + kV(3), 0.01 + kV(3)] ...
                 ,'CData',imread('concrete.jpg') ...
                 ,'FaceColor','texturemap');
             hold on;
-
             PlaceObject('Kitchen.ply',[kV(1)-2, kV(2)+4, kV(3)+0]);
+            PlaceObject('eStop.ply',[0,-0.46,1.7]);
+            PlaceObject('fireExtinguisherElevated.ply',[-4,-0.8,0.5]);
+            PlaceObject('female.ply',[1.4,-1,0]);
             %PlaceObject('plate_standing.ply', [-1, 4.2, 1.9]);
-
             %view(2);
             view(3);
-
             % %% Optional lighting for better visualization
             light('Position', [0, 0, 2], 'Style', 'local');
             light('Position', [0, 0, -2], 'Style', 'local');
-
         end
-
     end
 end
-
-
-% % %% Environment.Ananyatest
-% >>>>>>> 48ee822f9b0c3824178b544a3aa8b76332e785ab
-% %
-% % % Load up the enviroment
-% %
-% % clear all
-% % hold on
-% % axis equal
-% %
-% %
-% % surf([-5,5;-5,5] ...
-% % ,[5,5;-5,-5] ...
-% % ,[0,0;0,0] ...
-% % ,'CData',imread('concrete.jpg') ...
-% % ,'FaceColor','texturemap');
-% %
-% <<<<<<< HEAD
-% % h_1 = PlaceObject('Kitchen.ply',[0,2,0.9]);
-% % verts = [get(h_1,'Vertices'), ones(size(get(h_1,'Vertices'),1),1)];
-% % verts(:,1) = verts(:,1);
-% % set(h_1,'Vertices',verts(:,1:3))
-% =======
-% % h_1 = PlaceObject('Kitchen.PLY',[0,2,0.9]);
-% % verts = [get(h_1,'Vertices'), ones(size(get(h_1,'Vertices'),1),1)];
-% % verts(:,1) = verts(:,1);
-% % set(h_1,'Vertices',verts(:,1:3))
-%
