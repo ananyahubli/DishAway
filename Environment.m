6c18ba298e8f21de6d64726dbe926be1fc6c2079
No-raa <126116833+No-raa@users.noreply.github.com>
2023-11-09 21:00:26 +1100

    Minor edits

--- Environment.m
+++ Environment.m
@@ -2,6 +2,7 @@
 classdef Environment < handle
     properties
     end
+
     methods
         function self = Environment()
             disp('Setting up environment');
@@ -12,13 +13,16 @@ classdef Environment < handle
                 ,'CData',imread('concrete.jpg') ...
                 ,'FaceColor','texturemap');
             hold on;
+
             PlaceObject('Kitchen.ply',[kV(1)-2, kV(2)+4, kV(3)+0]);
             PlaceObject('eStop.ply',[0,-0.46,1.7]);
             PlaceObject('fireExtinguisherElevated.ply',[-4,-0.8,0.5]);
             PlaceObject('female.ply',[1.4,-1,0]);
             %PlaceObject('plate_standing.ply', [-1, 4.2, 1.9]);
             %view(2);
+
             view(3);
+            
             % %% Optional lighting for better visualization
             light('Position', [0, 0, 2], 'Style', 'local');
             light('Position', [0, 0, -2], 'Style', 'local');
